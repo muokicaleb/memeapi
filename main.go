965e1b6b7036fdf40ed43d550201cfa2789a678b
Muoki Caleb <muokicaleb@tutanota.com>
2021-10-24 19:19:12 +0300

    start

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+
+	"github.com/gin-gonic/gin"
+)
+
+var listAllMemes []string = listMemes()
+
+var memeServerBaseURl string = loadServerUrl()
+
+func main() {
+	router := gin.Default()
+	router.GET("/api/v3/meme", getMeme)
+
+	router.Run("localhost:8080")
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getMeme(c *gin.Context) {
+
+	memeUrl := memeServerBaseURl + listAllMemes[randomNumber()]
+	c.String(http.StatusOK, string(memeUrl))
+}
+
+func loadServerUrl() string {
+
+	envErr := godotenv.Load(".env")
+	if envErr != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv("MEME_SERVER")
+
+}
+
+func randomNumber() int {
+	rand.Seed(time.Now().UnixNano())
+	min := 0
+	max := len(listAllMemes)
+	return rand.Intn(max-min+1) + min
+
+}
+func listMemes() []string {
+	var files []string
+
+	fileInfo, err := ioutil.ReadDir("./memes")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range fileInfo {
+		files = append(files, file.Name())
+	}
+	return files
+
+}
